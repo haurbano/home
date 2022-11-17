6743cc5cab35c8c02e243f53245be55b154b4fbb
Hamilton Urbano <haurbano.dev@gmail.com>
2022-11-16 20:25:50 -0500

    Updates

---
+++ precache-manifest.12e2f27e6c8fb2057126c1c086a134d7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe3ad17c32503e81aaf4727665022f6",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "3da371d118b0220c5540",
+    "url": "/home/static/css/2.58570433.chunk.css"
+  },
+  {
+    "revision": "3e570b771f23ba77f887",
+    "url": "/home/static/css/main.97ee4f3a.chunk.css"
+  },
+  {
+    "revision": "3da371d118b0220c5540",
+    "url": "/home/static/js/2.2304e601.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home/static/js/2.2304e601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e570b771f23ba77f887",
+    "url": "/home/static/js/main.4228894f.chunk.js"
+  },
+  {
+    "revision": "7ad56a246e4cdcfc80ea",
+    "url": "/home/static/js/runtime-main.19c33384.js"
+  },
+  {
+    "revision": "62686fcfc741a6e56fd301e3d894771f",
+    "url": "/home/static/media/banner-2.62686fcf.jpg"
+  },
+  {
+    "revision": "031e1f562bf39d50c3fb373a1482e468",
+    "url": "/home/static/media/moni.031e1f56.jpg"
+  },
+  {
+    "revision": "161b152f89021bd2ab4b6ca7acca843e",
+    "url": "/home/static/media/profile.161b152f.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.202914f026e7dede100e34244c238240.js"
+  "/home/precache-manifest.12e2f27e6c8fb2057126c1c086a134d7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
